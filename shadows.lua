66c6e604a0396bda80b17eef637fda6f6433df64
Shiyo <strikinginsanity@hotmail.com>
2022-12-14 21:42:12 -0500

    Settings saving bugfix

--- shadows.lua
+++ shadows.lua
@@ -9,14 +9,17 @@ local chat = require('chat');
 local fonts = require('fonts');
 local settings = require('settings');
 
+local windowWidth = AshitaCore:GetConfigurationManager():GetFloat('boot', 'ffxi.registry', '0001', 1024);
+local windowHeight = AshitaCore:GetConfigurationManager():GetFloat('boot', 'ffxi.registry', '0002', 768);
+
 local default_settings = T{
 	font = T{
         visible = true,
         font_family = 'Arial',
         font_height = 30,
         color = 0xFFFFFFFF,
-        position_x = 1,
-        position_y = 1,
+        position_y = 800,
+        position_x = 1338,
 		background = T{
 			visible = true,
 			color = 0x80000000,
@@ -27,6 +30,13 @@ local default_settings = T{
 local shadows = T{
 	settings = settings.load(default_settings)
 };
+
+local UpdateSettings = function()
+  if (shadows.font ~= nil) then
+      shadows.font:destroy();
+  end
+  shadows.font = fonts.new(shadows.settings.font);
+end
 
 local function GetShadowCount()
     local me = AshitaCore:GetMemoryManager():GetPlayer()
@@ -46,18 +56,32 @@ local function GetShadowCount()
     return 0;
 end
 
+
 ashita.events.register('load', 'load_cb', function ()
-    shadows.font = fonts.new(shadows.settings.font);
+  shadows.font = fonts.new(shadows.settings.font);
+  settings.register('settings', 'settingchange', UpdateSettings);
 end);
+
 
 ashita.events.register('d3d_present', 'present_cb', function ()
  	local mJob = AshitaCore:GetMemoryManager():GetPlayer():GetMainJob();
 	local sJob = AshitaCore:GetMemoryManager():GetPlayer():GetSubJob();
+
+  local fontObject = shadows.font;
+  if (fontObject.position_x > windowWidth) then
+    fontObject.position_x = 0;
+  end
+  if (fontObject.position_y > windowHeight) then
+    fontObject.position_y = 0;
+  end
+  if (fontObject.position_x ~= shadows.settings.font.position_x) or (fontObject.position_y ~= shadows.settings.font.position_y) then
+      shadows.settings.font.position_x = fontObject.position_x;
+      shadows.settings.font.position_y = fontObject.position_y;
+      settings.save()
+  end
 	
 	if mJob == 13 or sJob == 13 then
 	  shadows.font.text = ('%d' ):fmt(GetShadowCount());    
-	  shadows.settings.font.position_x = shadows.font:GetPositionX();
-	  shadows.settings.font.position_y = shadows.font:GetPositionY();
 	  shadows.font.visible = true;
 	else
 	  shadows.font.visible = false;
